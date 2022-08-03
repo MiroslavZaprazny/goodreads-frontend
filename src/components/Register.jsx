7a8ebf164ad3e1cbe51932b68c40f0184f35c243
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2022-08-03 13:19:13 +0200

    Register validation

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [password_confirmation, setPasswordConfirmation] = useState('');
+  const [error, setError] = useState();
+  const [successMessage, setSuccessMessage] = useState();
 
   const handleSubmit = async () => {
-    let userInfo = { email, password };
+    let userInfo = { email, password, password_confirmation };
     let result = await fetch('http://127.0.0.1:8000/api/register', {
       method: 'POST',
       body: JSON.stringify(userInfo),
@@ -17,9 +20,20 @@ const Register = () => {
     });
     setEmail('');
     setPassword('');
+    setPasswordConfirmation('');
     result = await result.json();
-    console.log(result);
+    if (result.status != 200) {
+      setError(result.validate_err);
+    } else if (result.status == 200) {
+      setError('');
+      setSuccessMessage(result.message);
+    }
   };
+  useEffect(() => {
+    setTimeout(() =>{
+      setSuccessMessage('')
+    }, 5000)
+  }, [successMessage]);
   return (
     <div>
       <div className="container w-3/4 xl:w-1/3 shadow-sm border rounded-xl mx-auto space-y-6 py-8 px-6">
@@ -52,28 +66,45 @@ const Register = () => {
             className="rounded-lg border border-gray-300 text-gray-700 h-10 px-2 py-5"
             placeholder="Enter your email..."
           />
+          {error && (
+            <span className="text-sm text-red-500 mt-1 ml-2 transition ease-in duration-300">
+              {error.email}
+            </span>
+          )}
         </div>
         <div className="row flex flex-col w-full">
           <label className="font-semibold text-gray-800 mb-2">Password</label>
           <input
-            type="text"
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             name="password"
             className="rounded-lg border border-gray-300 text-gray-700 h-10 px-2 py-5"
             placeholder="Enter your password..."
           />
+          {error && (
+            <span className="text-sm text-red-500 mt-1 ml-2 transition ease-in duration-300">
+              {error.password}
+            </span>
+          )}
         </div>
         <div className="row flex flex-col w-full">
           <label className="font-semibold text-gray-800 mb-2">
             Repeat Password
           </label>
           <input
-            type="text"
-            name="password"
+            type="password"
+            name="password_confirmation"
+            value={password_confirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
             className="rounded-lg border border-gray-300 text-gray-700 h-10 px-2 py-5"
             placeholder="Repeat your password..."
           />
+          {error && (
+            <span className="text-sm text-red-500 mt-1 ml-2 transition ease-in duration-300">
+              {error.password_confirmation}
+            </span>
+          )}
         </div>
         <button
           onClick={handleSubmit}
@@ -81,6 +112,11 @@ const Register = () => {
         >
           Register
         </button>
+        {successMessage && (
+          <p className="mt-2 text-green-500 text-lg font-medium transition-all">
+            {successMessage}
+          </p>
+        )}
       </div>
     </div>
   );
